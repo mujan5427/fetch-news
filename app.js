43d94265e3dc4c0975ea472751b67f4ad4cbc0f6
Justin Ho <mujan5427@gmail.com>
2017-04-17 11:08:50 +0800

    fix issue

--- app.js
+++ app.js
@@ -12,48 +12,48 @@ request(
     , uri: 'http://www.storm.mg/article/249327'
     }
     , function (error, response, body) {
-    
+
       if (response.statusCode == 200) {
         console.log('status : ' + response.statusCode)
-        
+
         var article_start = body.split('<article>');
         var article_end = article_start[1].split('</article>');
         var article = article_end[0].split('<div class="ad_article_block">')[0];
-        
-        var $ = cheerio.load(`<article>${article}</article>`)
-        
-        
+
+        var $ = cheerio.load(article)
+
+
 
         // 每篇文章的圖片數量
         var article_img_count = $('.type-image').length;
-        
+
         for (var i = 1; i <= article_img_count; i++) {
-          
+
           var figure_area = `<figure><img src="${$(`.dnd-atom-wrapper:nth-of-type(1) img`).attr('src')}"><figcaption>${$(`.dnd-atom-wrapper:nth-of-type(1) .meta`).text()}</figcaption></figure>`
 
           console.log(i);
-        
+
           $(`.type-image:nth-of-type(1)`).replaceWith(figure_area);
-          
+
         }
-        
-        
+
+
         $('p').removeAttr('aid');
         $('h2').removeAttr('class')
-        
-        // console.log(htmlDncode($.html()));
-        
-        
+
+        console.log(htmlDncode($.html()));
+
+
         var sql = `insert into news (news_title, author, news_contetn) VALUES ('第三則新聞', '何宗軒', '${htmlDncode($.html())}')`;
 
         connection_db(sql);
-        
-        
+
+
       } else {
-        
+
         console.log('error: '+ response.statusCode)
         // console.log(body)
-        
+
       }
     }
 )
@@ -62,7 +62,7 @@ request(
 
 // app.get("/", function(req, res) {})
 
-  
+
 // 想要執行的 Sql statement
 // var sql = 'insert into news (news_title, author, news_contetn) VALUES ("第二則新聞", "何宗軒", "我是新聞文章")';
 
--- helpers/connection_db.js
+++ helpers/connection_db.js
@@ -1,24 +1,32 @@
 var mysql = require('mysql')
 
 var connection_db = function(sql_statment) {
-  
+
+  // Clound 9 DataBase Setting
+  // var connection = mysql.createConnection({
+  //   host     : 'localhost',
+  //   user     : 'justinho',
+  //   password : '',
+  //   database : 'c9'
+  // });
+
   var connection = mysql.createConnection({
     host     : 'localhost',
-    user     : 'justinho',
-    password : '',
-    database : 'c9'
+    user     : 'root',
+    password : '12345678',
+    database : 'my_mac_db'
   });
-    
+
   connection.connect()
-    
+
   connection.query(sql_statment, function (err, rows, fields) {
     if (err) throw err
-  
+
     console.log('result : ' + rows[0])
   })
-  
+
   connection.end()
-  
+
 }
- 
+
 module.exports = connection_db;
